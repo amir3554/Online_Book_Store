c1d5c631e159751702df0dfb39291c09e8bc3821
amir dwikat <amirdwikat@gmail.com>
2025-05-03 09:16:12 +0200

    adding models

--- oprations/models.py
+++ oprations/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from store.models import Product
 
-# Create your models here.
+class PaymentMethod(models.IntegerChoices):
+    PAYPAL = 1, 'PayPal'
+    STRIPE = 2, 'Stripe'
+
+class TransactionStatus(models.IntegerChoices):
+    PENDING = 0, 'Pending'
+    COMPLETED = 1, 'Completed'
+    CANCELED = 2, 'Canceled'
+
+
+class Transaction(models.Model):
+    session = models.CharField(max_length=255)
+    items = models.JSONField(default=dict)
+    customer = models.JSONField(default=dict)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.IntegerField(choices=TransactionStatus.choices, default=TransactionStatus.PENDING)
+    paymeny_method = models.IntegerField(choices=PaymentMethod.choices, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def customer_name(self):
+        return self.customer['first_name'] + ' ' + self.customer['last_name']
+    
+    @property
+    def customer_email(self):
+        return self.customer['email']
+
+
+
+
+class Order(models.Model):
+    transaction = models.OneToOneField(Transaction, on_delete=models.PROTECT, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return str(self.pk)
+
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,66 @@
 from django.db import models
+from django.contrib.sessions.models import Session
+from mystore_project import settings
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(null=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    featured = models.BooleanField(default=False)
+    order = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField(max_length=255, null=True)
+    description = models.TextField(null=True)
+    price = models.FloatField()
+    stock = models.PositiveIntegerField(null=True)
+    image = models.ImageField(upload_to='products_images/', null=True)
+    pdf_file = models.FileField(upload_to='products_files/')
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+
+    @property
+    def pdf_file_url(self):
+        return settings.MEDIA_ROOT + self.pdf_file.url
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.TextField(max_length=500)
+    image = models.ImageField(null=True)
+    order = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Cart(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    items = models.JSONField(default=True)
