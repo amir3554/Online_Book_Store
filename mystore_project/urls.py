2fe359447d424c8aa2777347489c851a2eb0d74b
amir dwikat <amirdwikat@gmail.com>
2025-05-14 20:06:58 +0200

    #

--- mystore_project/urls.py
+++ mystore_project/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
-from . import secret_info
 import os
 
 
